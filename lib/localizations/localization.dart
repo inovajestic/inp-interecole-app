455f72c859c1519c68f81bacd9b8ce7c3d7b1c36
mycs95 <samsonmolou@gmail.com>
2019-01-27 04:00:54 +0000

    add splash screen

--- lib/localizations/localization.dart
+++ lib/localizations/localization.dart
@@ -22,74 +22,137 @@ class ArchSampleLocalizations {
   }
 
   String get football => Intl.message(
-    'football',
-    name: 'football',
-    args: [],
-    locale: locale.toString(),
-  );
+        'football',
+        name: 'football',
+        args: [],
+        locale: locale.toString(),
+      );
 
   String get basketball => Intl.message(
-    'basketball',
-    name: 'basketball',
-    args: [],
-    locale: locale.toString(),
-  );
+        'basketball',
+        name: 'basketball',
+        args: [],
+        locale: locale.toString(),
+      );
 
   String get volleyball => Intl.message(
-    'volleyball',
-    name: 'volleyball',
-    args: [],
-    locale: locale.toString(),
-  );
+        'volleyball',
+        name: 'volleyball',
+        args: [],
+        locale: locale.toString(),
+      );
 
   String get handball => Intl.message(
-    'handball',
-    name: 'handball',
-    args: [],
-    locale: locale.toString(),
-  );
+        'handball',
+        name: 'handball',
+        args: [],
+        locale: locale.toString(),
+      );
 
   String get tiret => Intl.message(
-    ' - ',
-    name: 'tiret',
-    args: [],
-    locale: locale.toString(),
-  );
+        ' - ',
+        name: 'tiret',
+        args: [],
+        locale: locale.toString(),
+      );
 
   String get quartTemps => Intl.message(
-    'Quart-temps',
-    name: 'quartTemps',
-    args: [],
-    locale: locale.toString(),
-  );
+        'Quart-temps',
+        name: 'quartTemps',
+        args: [],
+        locale: locale.toString(),
+      );
 
   String get duree => Intl.message(
-    'Durée',
-    name: 'duree',
-    args: [],
-    locale: locale.toString(),
-  );
+        'Durée',
+        name: 'duree',
+        args: [],
+        locale: locale.toString(),
+      );
 
   String get terminee => Intl.message(
-    'Terminée',
-    name: 'terminee',
+        'Terminée',
+        name: 'terminee',
+        args: [],
+        locale: locale.toString(),
+      );
+
+  String get about => Intl.message(
+        'A propos',
+        name: 'about',
+        args: [],
+        locale: locale.toString(),
+      );
+
+  String get descInova => Intl.message(
+        '« Inova – La Junior Entreprise STIC » association à vocation économique et pédagogique, et à but non lucratif, des étudiants de l’association des Ingénieurs STIC',
+        name: 'descInova',
+        args: [],
+        locale: locale.toString(),
+      );
+
+  String get inovaEmail => Intl.message(
+    'inova.jestic@gmail.com',
+    name: 'inovaEmail',
     args: [],
     locale: locale.toString(),
   );
 
-  String get about => Intl.message(
-    'A propos',
-    name: 'about',
+  String get inovaContact1 => Intl.message(
+    '+225 49 63 12 23',
+    name: 'inovaContact1',
+    args: [],
+    locale: locale.toString(),
+  );
+
+  String get inovaContact2 => Intl.message(
+    '+225 44 28 62 65',
+    name: 'inovaContact2',
+    args: [],
+    locale: locale.toString(),
+  );
+
+  String get developer => Intl.message(
+        'Développeur :',
+        name: 'developer',
+        args: [],
+        locale: locale.toString(),
+      );
+
+  String get developerName => Intl.message(
+    'MOLOU Yao Clair Samson',
+    name: 'developerName',
+    args: [],
+    locale: locale.toString(),
+  );
+
+  String get developerStudy => Intl.message(
+    'Elève-Ingénieur en STIC / INFO 3',
+    name: 'developerName',
+    args: [],
+    locale: locale.toString(),
+  );
+
+  String get contact => Intl.message(
+    'Contacts :',
+    name: 'contacts',
+    args: [],
+    locale: locale.toString(),
+  );
+
+  String get developerEmail => Intl.message(
+    'samsonmolou@gmail.com',
+    name: 'developerEmail',
     args: [],
     locale: locale.toString(),
   );
 
   String get enCours => Intl.message(
-    'En cours',
-    name: 'enCours',
-    args: [],
-    locale: locale.toString(),
-  );
+        'En cours',
+        name: 'enCours',
+        args: [],
+        locale: locale.toString(),
+      );
 }
 
 class ArchSampleLocalizationsDelegate
--- lib/ui/about_screen.dart
+++ lib/ui/about_screen.dart
@@ -2,24 +2,101 @@ import 'package:flutter/material.dart';
 import 'package:inp_interecole/interecole.dart';
 
 class AboutScreen extends StatelessWidget {
+  final double _imageHeight = 256.0;
+
   AboutScreen({
     Key key,
   }) : super(key: key ?? ArchSampleKeys.aboutScreen);
 
   @override
-  State createState() {
-
-  }
-
-  @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          ArchSampleLocalizations.of(context).about
-        ),
+        title: Text(ArchSampleLocalizations.of(context).about),
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(3.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.asset(
+                    'assets/logo_inova.png',
+                    fit: BoxFit.cover,
+                    scale: 10.0,
+                  ),
+                ],
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).descInova,
+                textAlign: TextAlign.center,
+                style: theme.textTheme.body2,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 2.0),
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).contact,
+                style: theme.textTheme.title,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 2.0),
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).inovaContact1,
+                style: theme.textTheme.subtitle,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 2.0),
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).inovaContact2,
+                style: theme.textTheme.subtitle,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 2.0),
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).inovaEmail,
+                style: theme.textTheme.subtitle,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 15.0, bottom: 2.0),
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).developer,
+                style: theme.textTheme.title,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 5.0, bottom: 2.0),
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).developerName,
+                style: theme.textTheme.subtitle,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 2.0, bottom: 2.0),
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).developerStudy,
+                style: theme.textTheme.subtitle,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 2.0, bottom: 2.0),
+              ),
+              Text(
+                ArchSampleLocalizations.of(context).developerEmail,
+                style: theme.textTheme.subtitle,
+              ),
+            ],
+          ),
+        ),
+      )
     );
   }
 }
-
